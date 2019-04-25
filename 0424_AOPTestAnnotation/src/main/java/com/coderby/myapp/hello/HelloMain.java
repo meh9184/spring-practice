0482e988e8fbbf281f528c06c70f3d08456824cf
Eunhwan Moon <meh9184@naver.com>
2019-04-25 13:29:41 +0900

    소스코드 에러 디버깅

--- 0424_AOPTestAnnotation/src/main/java/com/coderby/myapp/hello/HelloMain.java
+++ 0424_AOPTestAnnotation/src/main/java/com/coderby/myapp/hello/HelloMain.java
@@ -1,19 +1,29 @@
 package com.coderby.myapp.hello;
 
 import org.springframework.context.support.GenericXmlApplicationContext;
+
 import com.coderby.myapp.hello.controller.HelloController;
+import com.coderby.myapp.hello.service.HelloService;
+import com.coderby.myapp.hello.service.IHelloService;
 
 public class HelloMain {
 
 	public static void main(String[] args) {
+
+		
+		String[] path = {"application-config.xml"};
+		
 		GenericXmlApplicationContext context = 
-				new GenericXmlApplicationContext("application-config.xml");
+				new GenericXmlApplicationContext(path);
 		
 		HelloController controller = 
 				context.getBean("helloController", HelloController.class);
 		
 		controller.Hello("문은환");
-//		controller.Goodbye("문은환");
+		
+		System.out.println("===================");
+		
+		controller.Goodbye("문은환");
 		
 		context.close();
 	}
