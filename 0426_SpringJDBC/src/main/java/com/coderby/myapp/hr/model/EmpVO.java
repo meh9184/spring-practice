24c3a486d475739677574c9924a8de594d4a0a99
Eunhwan Moon <meh9184@naver.com>
2019-04-26 14:40:53 +0900

    VO 클래스 추가

---
+++ 0426_SpringJDBC/src/main/java/com/coderby/myapp/hr/model/EmpVO.java
@@ -0,0 +1,92 @@
+package com.coderby.myapp.hr.model;
+
+import java.util.Date;
+
+public class EmpVO {
+	int employeeId;
+	String firstName;
+	String lastName;
+	String email;
+	String phoneNumber;
+	Date hireDate;
+	String jobId;
+	double salary;
+	double commissionPct;
+	int managerId;
+	int departmentId;
+	
+	@Override
+	public String toString() {
+		return "EmpVO [employeeId=" + employeeId + ", firstName=" + firstName + ", lastName=" + lastName + ", email="
+				+ email + ", phoneNumber=" + phoneNumber + ", hireDate=" + hireDate + ", jobId=" + jobId + ", salary="
+				+ salary + ", commissionPct=" + commissionPct + ", managerId=" + managerId + ", departmentId="
+				+ departmentId + "]";
+	}
+	
+	public int getEmployeeId() {
+		return employeeId;
+	}
+	public void setEmployeeId(int employeeId) {
+		this.employeeId = employeeId;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	public Date getHireDate() {
+		return hireDate;
+	}
+	public void setHireDate(Date hireDate) {
+		this.hireDate = hireDate;
+	}
+	public String getJobId() {
+		return jobId;
+	}
+	public void setJobId(String jobId) {
+		this.jobId = jobId;
+	}
+	public double getSalary() {
+		return salary;
+	}
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+	public double getCommissionPct() {
+		return commissionPct;
+	}
+	public void setCommissionPct(double commissionPct) {
+		this.commissionPct = commissionPct;
+	}
+	public int getManagerId() {
+		return managerId;
+	}
+	public void setManagerId(int managerId) {
+		this.managerId = managerId;
+	}
+	public int getDepartmentId() {
+		return departmentId;
+	}
+	public void setDepartmentId(int departmentId) {
+		this.departmentId = departmentId;
+	}
+}
