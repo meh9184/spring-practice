bacf6434fd05d1d28046a99ea5943161d7bda7f9
Eunhwan Moon <meh9184@naver.com>
2019-04-30 15:08:08 +0900

    어노테이션을 이용한 트랜젝션 적용

---
+++ 0430_SpringJDBCTransactionAnnotation/src/main/java/com/coderby/myapp/hr/dao/IEmpRepository.java
@@ -0,0 +1,18 @@
+package com.coderby.myapp.hr.dao;
+
+import java.util.List;
+
+import com.coderby.myapp.hr.model.EmpVO;
+
+
+public interface IEmpRepository {
+	int getEmpCount();
+	int getEmpCount(int deptId);
+	List<EmpVO> getEmpList();
+	EmpVO getEmpInfo(int empId);
+	void insertEmp(EmpVO emp);
+	void updateEmp(EmpVO emp);
+	void deleteEmp(int empId, String email);
+	void deleteJobHistory(int empId);
+	
+}
