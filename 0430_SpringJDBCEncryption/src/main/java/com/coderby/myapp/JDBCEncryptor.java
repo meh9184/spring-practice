356257f258b7844ad8aa0c8f2a6e6d1d34307f2f
Eunhwan Moon <meh9184@naver.com>
2019-05-01 13:09:22 +0900

    암호화 된 코드 jdbc.properties 파일에 입력

--- 0430_SpringJDBCEncryption/src/main/java/com/coderby/myapp/JDBCEncryptor.java
+++ 0430_SpringJDBCEncryption/src/main/java/com/coderby/myapp/JDBCEncryptor.java
@@ -6,9 +6,9 @@ public class JDBCEncryptor {
 	public static void main(String[] args) {
 		StandardPBEStringEncryptor enc = new StandardPBEStringEncryptor();
 		enc.setPassword("ansdmsghks");
-		System.out.println("oracle.jdbc.driver.OracleDriver");
-		System.out.println("jdbc:oracle:thin:@localhost:1521:xe");
-		System.out.println("hr");
-		System.out.println("hr");
+		System.out.println(enc.encrypt("oracle.jdbc.driver.OracleDriver"));
+		System.out.println(enc.encrypt("jdbc:oracle:thin:@localhost:1521:xe"));
+		System.out.println(enc.encrypt("hr"));
+		System.out.println(enc.encrypt("hr"));
 	}
 }
