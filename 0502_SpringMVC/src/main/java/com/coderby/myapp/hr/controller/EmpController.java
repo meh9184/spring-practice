e00e6777033bb5322be8238c5cb6c6b9a385feeb
Eunhwan Moon <meh9184@naver.com>
2019-05-02 14:42:23 +0900

    url restful 하게 디자인한 컨트롤러

--- 0502_SpringMVC/src/main/java/com/coderby/myapp/hr/controller/EmpController.java
+++ 0502_SpringMVC/src/main/java/com/coderby/myapp/hr/controller/EmpController.java
@@ -3,6 +3,7 @@ package com.coderby.myapp.hr.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -14,10 +15,20 @@ public class EmpController {
 	@Autowired
 	IEmpService empService;
 
-	@RequestMapping(value="/hr/count")
-	public String empCount(@RequestParam(
-		value="deptid", required=false, defaultValue="0") int deptid, 
-			Model model) {
+//	@RequestMapping(value="/hr/count")
+//	public String empCount(@RequestParam(
+//		value="deptid", required=false, defaultValue="0") int deptid, 
+//			Model model) {
+//		if(deptid==0) {
+//			model.addAttribute("count", empService.getEmpCount());
+//		}else {
+//			model.addAttribute("count", empService.getEmpCount(deptid));
+//		}
+//		return "hr/count";
+//	}
+
+	@RequestMapping(value="/hr/count/{deptid}")
+	public String empCount(@PathVariable int deptid, Model model) {
 		if(deptid==0) {
 			model.addAttribute("count", empService.getEmpCount());
 		}else {
