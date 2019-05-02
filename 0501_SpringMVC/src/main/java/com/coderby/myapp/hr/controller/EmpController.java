047d3b7cc96ee8f8756bb6a4734df07d5ee3c8ea
Eunhwan Moon <meh9184@naver.com>
2019-05-02 13:21:47 +0900

    context root 문제 해결 : project properties 부분 /로 변경

--- 0501_SpringMVC/src/main/java/com/coderby/myapp/hr/controller/EmpController.java
+++ 0501_SpringMVC/src/main/java/com/coderby/myapp/hr/controller/EmpController.java
@@ -24,7 +24,7 @@ public class EmpController {
 			model.addAttribute("count", empService.getEmpCount(deptId));
 		}
 		
-		return "hr/count";
+		return "/hr/count";
 	}
 	
 }
---
+++ 0502_SpringMVC/src/main/java/com/coderby/myapp/hr/dao/EmpRepository.java
@@ -0,0 +1,130 @@
+package com.coderby.myapp.hr.dao;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+import com.coderby.myapp.hr.model.EmpVO;
+
+@Repository
+public class EmpRepository implements IEmpRepository {
+
+	//鍮� 而⑦뀒�씠�꼫�뿉�꽌 DB�뿉 �뿰寃고븷 �닔 �엳�뒗 而ㅻ꽖�뀡 媛앹껜 媛��졇�삤湲�
+	@Autowired
+	JdbcTemplate jdbcTemplate;
+	
+	//RowMapper 援ы쁽 �겢�옒�뒪 �꽕�젙
+	private class EmpMapper implements RowMapper<EmpVO>{
+		@Override
+		public EmpVO mapRow(ResultSet rs, int rowNum) throws SQLException {
+			EmpVO emp = new EmpVO();
+			emp.setEmployeeId(rs.getInt("employee_id"));
+			emp.setFirstName(rs.getString("first_name"));
+			emp.setLastName(rs.getString("last_name"));
+			emp.setEmail(rs.getString("email"));
+			emp.setPhoneNumber(rs.getString("phone_number"));
+			emp.setHireDate(rs.getDate("hire_date"));
+			emp.setJobId(rs.getString("job_id"));
+			emp.setSalary(rs.getDouble("salary"));
+			emp.setCommissionPct(rs.getDouble("commission_pct"));
+			emp.setManagerId(rs.getInt("manager_id"));
+			emp.setDepartmentId(rs.getInt("department_id"));
+			return emp;
+		}
+		
+	}
+	
+	
+	
+	@Override
+	public int getEmpCount() {
+		String sql = "SELECT COUNT(*) FROM employees";
+		return jdbcTemplate.queryForObject(sql, Integer.class);
+	}
+
+	public int getEmpCount(int deptid) {
+		String sql = "SELECT COUNT(*) FROM EMPLOYEES WHERE department_id=?";
+		return jdbcTemplate.queryForObject(sql, Integer.class, deptid);
+	}
+
+	@Override
+	public List<EmpVO> getEmpList() {
+		String sql = "SELECT * FROM EMPLOYEES";
+		return jdbcTemplate.query(sql, new EmpMapper());
+	}
+
+	@Override
+	public EmpVO getEmpInfo(int empid) {
+		String sql = "SELECT * FROM EMPLOYEES WHERE employee_id=?";
+		return jdbcTemplate.queryForObject(sql, new EmpMapper(), empid);
+	}
+
+	@Override
+	public void insertEmp(EmpVO emp) {
+		String sql = "INSERT INTO EMPLOYEES VALUES (?,?,?,?,?,SYSDATE,?,?,?,?,?)";
+		jdbcTemplate.update(sql, 
+				emp.getEmployeeId(),
+				emp.getFirstName(),
+				emp.getLastName(),
+				emp.getEmail(),
+				emp.getPhoneNumber(),
+				emp.getJobId(),
+				emp.getSalary(),
+				emp.getCommissionPct(),
+				emp.getManagerId(),
+				emp.getDepartmentId());
+	}
+
+	@Override
+	public void updateEmp(EmpVO emp) {
+		String sql= "UPDATE employees SET "
+				+"first_name=?, last_name=?, email=?, "
+				+"phone_number=?, hire_date=?, job_id=?, "
+				+"salary=?, commission_pct=?, manager_id=?, "
+				+"department_id=? WHERE employee_id=?";
+		jdbcTemplate.update(sql, 
+				emp.getFirstName(),
+				emp.getLastName(),
+				emp.getEmail(),
+				emp.getPhoneNumber(),
+				emp.getHireDate(),
+				emp.getJobId(),
+				emp.getSalary(),
+				emp.getCommissionPct(),
+				emp.getManagerId(),
+				emp.getDepartmentId(),
+				emp.getEmployeeId()
+				);
+	}
+
+	@Override
+	public void deleteEmp(int empid, String email) {
+		String sql = "DELETE FROM employees WHERE employee_id=? AND email=?";
+		jdbcTemplate.update(sql, empid, email);
+	}
+
+	@Override
+	public void deleteJobHistory(int empid) {
+		String sql = "DELETE FROM job_history WHERE employee_id=?";
+		jdbcTemplate.update(sql, empid);
+	}
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
